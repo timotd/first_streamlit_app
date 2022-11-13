84f7404f1e6fc5cb5f4f8463003d66102f2570c7
Timo Lappalainen <118112852+timotd@users.noreply.github.com>
2022-11-13 19:52:21 +0200

    Update streamlit_app.py
    Added Menu and items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.header("Kele, Spinach & Rocket Smoothie")
+streamlit.header("Hard-Boiled Free-Range Egg")
