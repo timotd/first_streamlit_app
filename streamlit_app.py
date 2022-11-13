d2585e593d64d28931a1aaedf8d71ed4ee36717f
Timo Lappalainen <118112852+timotd@users.noreply.github.com>
2022-11-13 19:53:13 +0200

    Update streamlit_app.py
    Fix typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.header("Kele, Spinach & Rocket Smoothie")
-streamlit.header("Hard-Boiled Free-Range Egg")
+streamlit.text("Kele, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
